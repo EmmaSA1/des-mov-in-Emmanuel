34af1b09531498652e5d6056d85d3701eafa1b85
EmmaSA1 <132949371+EmmaSA1@users.noreply.github.com>
2025-09-24 23:23:38 -0600

    Navegacion de una pagina a otra

---
+++ lib/SecondPages.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Welcome to the Second Page!",
+              style: TextStyle(fontSize: 22),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Back to Home"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
